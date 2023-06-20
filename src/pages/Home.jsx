97f06d3836451309ddfd27d5be107fa636273db2
Jeni Hoyt <ziahrahl@gmail.com>
2023-06-20 11:59:29 -0400

    add images for each category

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,25 @@ const Home = () => {
   const { user } = useAuth();
   const name = user?.email.split("@")[0] || "Guest user";
   return (
-    <div className="mt-10 flex flex-col justify-center items-center">
-      <h1>Hi {name}!</h1>
-      <div>
-        <p>Looking for a yummy bowl recipe?</p>
-        <Link to="/bowls">Bowl recipes</Link>
+    <section className="">
+      <div className="">
+        <img
+          srcset="food-prep-400w.jpg 400w, food-prep-640w.jpg 640w"
+          sizes="(max-width: 600px) 400px, 640px"
+          src="food-prep-640w.jpg"
+          alt="Food preparation"
+          className=""
+        />
+        <div className="flex flex-col justify-center items-center">
+          <h1>Hi {name}!</h1>
+
+          <div>
+            <p>Looking for a yummy bowl recipe?</p>
+            <Link to="/bowls">Bowl recipes</Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
